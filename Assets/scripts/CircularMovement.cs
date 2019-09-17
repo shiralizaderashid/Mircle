cff05f634f896ccc1330969bd494216fb48cdb57
DerSkythe <>
2019-09-17 18:08:27 +0400

    First commit

---
+++ Assets/scripts/CircularMovement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class CircularMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform rotationCenter;
+
+    [SerializeField]
+    float angularSpeed = 2f;
+
+    private float rotationRadius;
+
+   
+
+    float posX, posY, angle = 0f;
+
+    private float circleDistance;
+
+
+    void Start()
+    {
+
+        rotationRadius = Vector3.Distance(transform.position, rotationCenter.position);
+    }
+
+
+	void FixedUpdate () {
+		posX = rotationCenter.position.x + Mathf.Cos (angle) * rotationRadius;
+		posY = rotationCenter.position.y + Mathf.Sin (angle) * rotationRadius;
+		transform.position = new Vector2 (posX, posY);
+		angle = angle + Time.deltaTime * angularSpeed;
+
+		if (angle >= 360f)
+			angle = 0f;
+	}
+
+}
---
+++ Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform rotationCenter;
+
+    [SerializeField]
+    Transform circleTransform;
+
+
+    private Vector2 originalPos;
+
+    private Vector2 targetposition = Vector2.zero;
+
+    void Start()
+    {
+        originalPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        Debug.Log("original " + gameObject.transform.position.x + "y="+gameObject.name + gameObject.transform.position.y);
+        targetposition = new Vector2(rotationCenter.position.x + 0.1f, rotationCenter.position.y + 0.1f);
+        Debug.Log("pozsiya"+ targetposition.x + "y="+targetposition.y);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if ((Vector2)transform.position != targetposition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetposition, 0.5f * Time.deltaTime);
+        }
+       
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag=="circle")
+        {
+            transform.position = Vector2.MoveTowards(transform.position, originalPos, 0.5f * Time.deltaTime);
+        }
+    }
+    
+}
